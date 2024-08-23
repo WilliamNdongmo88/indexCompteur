bbcf0bbf6939f1e7b4473297ad9966b9ad2b14bc
unknown <WilliamNdongmo88>
2024-08-24 01:27:43 +0200

    index compteur

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ app.use((req, res, next) => {
         {
             "reference": "4852916374",
             "index": 871,
-            "dernier_date_releve_index": "Lundi 18 Avril 2022"
+            "derniere_date_releve_index": "18/05/2022"
           },
           {
             "reference": "9632741850",
             "index": 345,
-            "dernier_date_releve_index": "Dimanche 9 Septembre 2021"
+            "derniere_date_releve_index": "09/09/2024"
           },
           {
             "reference": "7418529630",
             "index": 562,
-            "dernier_date_releve_index": "Vendredi 3 Février 2023"
+            "derniere_date_releve_index": "03/02/2023"
           },
           {
             "reference": "2963741580",
             "index": 725,
-            "dernier_date_releve_index": "Jeudi 14 Novembre 2022"
+            "derniere_date_releve_index": "14/11/2022"
           },
           {
             "reference": "1482963750",
             "index": 499,
-            "dernier_date_releve_index": "Mardi 22 Juin 2021"
+            "derniere_date_releve_index": "22/06/2023"
           }
     ]); 
  });
